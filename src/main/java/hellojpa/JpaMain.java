c2d35f76ba6ca2f9d8010f95eb2af4fabb96a487
JoyDaheeCha <daheecha0612@gmail.com>
2021-12-29 23:24:18 +0900

    refactor: try-catch-finally로 저장/롤백 로직 이동

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,16 +13,18 @@ public class JpaMain {
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+            em.persist(member);
 
-        Member member = new Member();
-        member.setId(2L);
-        member.setName("HelloB");
-        em.persist(member);
-
-        tx.commit();
-
-        em.close();
-
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
