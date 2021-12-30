4be348888da8db4c24bbb0751148c960fd234a16
JoyDaheeCha <daheecha0612@gmail.com>
2021-12-30 22:06:41 +0900

    commit: [준영속상태] em.clear

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,9 +17,10 @@ public class JpaMain {
         try {
             // 영속 엔티티 조회
             Member member1 = em.find(Member.class, 150L);
-            member1.setName("AA4");
 
-            em.detach(member1);
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L); // 영속성이 지워졌으므로 select 쿼리 총 2번 나감
 
             System.out.println("=============");
             tx.commit();
