ae7bca0d6d72994d7bdb940d382cde4ea3d60df3
JoyDaheeCha <daheecha0612@gmail.com>
2021-12-30 13:02:19 +0900

    commit: member 조회 로직 추가

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,11 +14,9 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
-            em.persist(member);
-
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
